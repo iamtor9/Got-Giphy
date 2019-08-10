64ae9815f5884f85116e71f79f33016cbf8c85f5
iamtor9 <iamtor9@gmail.com>
2019-08-10 14:32:54 -0500

    button id

--- giphy.js
+++ giphy.js
@@ -1,1 +1,16 @@
 const Topics = ["spring","summer", "winter", "fall", "rain", "snow", "sunshine", "storms"]
+
+
+
+
+
+
+
+//Add a form to your page that takes a value from a user input box and adds it to your `topics` array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
+<form action=""></form>
+
+let input = document.querySelector('input');
+input.value = 'type here'
+
+let submit = document.querySelector('input[type="submit"]');
+submit.value="send"
